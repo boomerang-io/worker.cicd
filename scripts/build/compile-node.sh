04d61f9e9efaadaa53b0e89588c7758830f185be
TYSON W LAWRIE <twlawrie@us.ibm.com>
2021-07-21 09:24:08 +1000

    Removing duplicate NODE_OPTIONS

--- scripts/build/compile-node.sh
+++ scripts/build/compile-node.sh
@@ -36,8 +36,6 @@ else
     echo "Setting Cypress Install Binary to $CYPRESS_INSTALL_BINARY..."
 fi
 
-export NODE_OPTIONS="--max-old-space-size=8192"
-
 if [ "$BUILD_TOOL" == "npm" ] || [ "$BUILD_TOOL" == "yarn" ]; then
     if [ -e 'yarn.lock' ]; then
         echo "Running YARN install..."
