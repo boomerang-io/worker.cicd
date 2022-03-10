740db08a7712e9aa09daf4cbff2c38e88dacdffa
Ming Xia Shi <mingxias@cn.ibm.com>
2022-03-10 16:08:46 +0800

    update script for skopeo configuration

--- scripts/deploy/containerregistry.sh
+++ scripts/deploy/containerregistry.sh
@@ -82,6 +82,10 @@ fi
 
 sleep 10
 
+echo "Updating skopeo configuration..."
+sed -i "s/default-docker:/default-docker:\n  sigstore: https:\/\/$GLOBAL_REGISTRY_HOST\/artifactory\/boomeranglib-docker/g" /etc/containers/registries.d/default.yaml
+cat /etc/containers/registries.d/default.yaml
+echo ""
 echo "Copying from Origin to Destination..."
 echo "- Origin: $GLOBAL_DOCKER_SERVER/$IMAGE_PATH/$IMAGE_NAME:$IMAGE_VERSION"
 echo "- Destination: $DESTINATION_DOCKER_SERVER$DESTINATION_REGISTRY_IMAGE_PATH/$IMAGE_NAME:$IMAGE_VERSION"
