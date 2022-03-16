949ed760145df30001383cb44a5b3287698a35a7
Ming Xia Shi <mingxias@cn.ibm.com>
2022-03-16 10:58:13 +0800

    update for trigger

--- scripts/deploy/containerregistry.sh
+++ scripts/deploy/containerregistry.sh
@@ -95,7 +95,6 @@ EOF
     cat codesign/default.yaml
     SKOPEO_OPTS+="--registries.d codesign/"
 fi
-
 echo ""
 echo "Copying from Origin to Destination..."
 echo "- Origin: $GLOBAL_DOCKER_SERVER/$IMAGE_PATH/$IMAGE_NAME:$IMAGE_VERSION"
