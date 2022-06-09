799486b2db463d3a14721654641308a80a39847e
Glen Hickman <gchickma@us.ibm.com>
2022-06-09 12:12:12 -0500

    address review points

--- scripts/common/initialize-dependencies-python.sh
+++ scripts/common/initialize-dependencies-python.sh
@@ -10,23 +10,19 @@ if [ "$DEBUG" == "true" ]; then
 fi
 
 if [ "$BUILD_LANGUAGE_VERSION" == "2" ]; then
-    # echo "Installing Python 2 ..."
-    # apk add python python-dev py-pip
-    #
-    # pip install --upgrade pip
     echo "Python 2 no longer supported ..."
     exit 89
 elif [ "$BUILD_LANGUAGE_VERSION" == "3" ]; then
-    echo "Installing Python 3 ..."
+    echo "Installing Python 3..."
     apk add python3 python3-dev py3-pip
 
     pip3 install --upgrade pip
 else
-    echo "Defaulting to and installing Python 3 ..."
+    echo "Defaulting to and installing Python 3..."
     apk add python3 python3-dev py3-pip
 
     pip3 install --upgrade pip
 fi
 
-echo "Installing additional tools & libraries ..."
+echo "Installing additional tools & libraries..."
 apk add gcc g++ libffi libffi-dev
