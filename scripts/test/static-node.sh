f45340ff9b3e9ccc49220ed1624357bec857c9fb
Glen Hickman <gchickma@us.ibm.com>
2022-10-18 15:02:36 -0500

    minor fixes to Node.js static test

--- scripts/test/static-node.sh
+++ scripts/test/static-node.sh
@@ -55,10 +55,9 @@ curl --insecure -o /opt/sonarscanner.zip -L -u $ART_USER:$ART_PASSWORD $ART_URL/
 unzip -o /opt/sonarscanner.zip -d /opt
 SONAR_FOLDER=`ls /opt | grep sonar-scanner`
 SONAR_HOME=/opt/$SONAR_FOLDER
+SONAR_FLAGS=
 if [ "$DEBUG" == "true" ]; then
     SONAR_FLAGS="-Dsonar.verbose=true"
-else
-    SONAR_FLAGS=
 fi
 
 # Run linting script
@@ -67,13 +66,13 @@ if [[ "$SCRIPT" != "undefined" ]]; then
     npm run lint
     SONAR_FLAGS="$SONAR_FLAGS -Dsonar.eslint.reportPaths=lint-report.json"
     ls -al lint-report.json
-    echo "SONAR_FLAGS=$SONAR_FLAGS"
 fi
+echo "SONAR_FLAGS=$SONAR_FLAGS"
 
 SRC_FOLDER=
 if [ -d "dist" ]; then
     echo "Source folder 'dist' exists."
-    SRC_FOLDER=src
+    SRC_FOLDER=dist
 elif [ -d "src" ]; then
     echo "Source folder 'src' exists."
     SRC_FOLDER=src
@@ -81,12 +80,18 @@ else
     echo "Source folder 'src' does not exist - defaulting to root folder of project and will scan all sub-folders."
     SRC_FOLDER=.
 fi
+echo "SRC_FOLDER=$SRC_FOLDER"
+
+SONAR_EXCLUSIONS=
+# Place setter for new IF statement to handle user specified exclusions
+SONAR_EXCLUSIONS="$SONAR_EXCLUSIONS -Dsonar.exclusions=/node_modules/**,/commands/**,/scripts/**"
+echo "SONAR_EXCLUSIONS=$SONAR_EXCLUSIONS"
 
 # Set Node.js bin path
 NODE_PATH=$(which node)
 echo "NODE_PATH=$NODE_PATH"
 
-$SONAR_HOME/bin/sonar-scanner -Dsonar.host.url=$SONAR_URL -Dsonar.sources=$SRC_FOLDER -Dsonar.login=$SONAR_APIKEY -Dsonar.projectKey=$COMPONENT_ID -Dsonar.projectName="$COMPONENT_NAME" -Dsonar.projectVersion=$VERSION_NAME -Dsonar.nodejs.executable=$NODE_PATH -Dsonar.scm.disabled=true -Dsonar.javascript.node.maxspace=8192 -Dsonar.exclusions=**/node_modules/** $SONAR_FLAGS
+$SONAR_HOME/bin/sonar-scanner -Dsonar.host.url=$SONAR_URL -Dsonar.sources=$SRC_FOLDER -Dsonar.login=$SONAR_APIKEY -Dsonar.projectKey=$COMPONENT_ID -Dsonar.projectName="$COMPONENT_NAME" -Dsonar.projectVersion=$VERSION_NAME -Dsonar.nodejs.executable=$NODE_PATH -Dsonar.scm.disabled=true -Dsonar.javascript.node.maxspace=8192 $SONAR_EXCLUSIONS $SONAR_FLAGS
 
 EXIT_CODE=$?
 echo "EXIT_CODE=$EXIT_CODE"
