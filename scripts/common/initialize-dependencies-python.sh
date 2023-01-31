27d0b0b38e9d67226aa28e93bf4dc9fe4d2c5cd6
Glen Hickman <gchickma@us.ibm.com>
2023-01-31 14:38:35 -0600

    readding pyreadline fix

--- scripts/common/initialize-dependencies-python.sh
+++ scripts/common/initialize-dependencies-python.sh
@@ -20,9 +20,10 @@ elif [ "$BUILD_LANGUAGE_VERSION" == "3" ]; then
     pip3 install --upgrade pip
 
     # Workaround for python bug:  AttributeError: module 'collections' has no attribute 'Callable'
+    pip3 uninstall -y pyreadline
+    pip3 install pyreadline3
     pip3 uninstall -y nose
     pip3 install nose-py3
-    # pip3 install -U nose --no-binary :all:
 else
     echo "Defaulting to and installing Python 3..."
     apt-get install -y python3-pip
@@ -31,9 +32,10 @@ else
     pip3 install --upgrade pip
 
     # Workaround for python bug:  AttributeError: module 'collections' has no attribute 'Callable'
+    pip3 uninstall -y pyreadline
+    pip3 install pyreadline3
     pip3 uninstall -y nose
     pip3 install nose-py3
-    # pip3 install -U nose --no-binary :all:
 fi
 
 echo "Installing additional tools & libraries..."
