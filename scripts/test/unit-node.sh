28fe6f48a20a9faa6546edb73c042069a51d2bec
Tim Bula <timrbula@gmail.com>
2023-02-08 16:20:32 -0600

    Update unit-node.sh

--- scripts/test/unit-node.sh
+++ scripts/test/unit-node.sh
@@ -101,26 +101,25 @@ fi
 # Check that vitest exists and that it is being used in the script
 if [[ -d "./node_modules/vitest" && "$SCRIPT" == *vitest* ]]; then
     TEST_REPORTER="vitest-sonar-reporter"
-    TEST_REPORTER_PATH="nodes_modules/vitest-sonar-reporter"
     SONAR_FLAGS="$SONAR_FLAGS -Dsonar.testExecutionReportPaths=test-report.xml"
     SONAR_FLAGS="$SONAR_FLAGS -Dsonar.tests=src"
 
     # Support Typscript and other common naming standards
     SONAR_FLAGS="$SONAR_FLAGS -Dsonar.test.inclusions=**/*.test.tsx,**/*.test.ts,**/*.test.jsx,**/*.test.js,**/*.spec.tsx,**/*.spec.ts,**/*.spec.js,**/*.spec.tsx"
     if [[ "$USE_NPM" == true ]]; then
-        COMMAND_ARGS="-- --reporter $TEST_REPORTER_PATH --outputFile test-report.xml"
+        COMMAND_ARGS="-- --reporter $TEST_REPORTER --outputFile test-report.xml"
         if [[ -d "./node_modules/vitest-sonar-reporter" ]]; then
             echo "Installing $TEST_REPORTER"
             npm i -D $TEST_REPORTER
         fi
     elif [[ "$USE_YARN" == true ]]; then
-        COMMAND_ARGS="--reporter $TEST_REPORTER_PATH --outputFile test-report.xml"
+        COMMAND_ARGS="--reporter $TEST_REPORTER --outputFile test-report.xml"
         if [[ -d "./node_modules/vitest-sonar-reporter" ]]; then
             echo "Installing $TEST_REPORTER"
             yarn add -D $TEST_REPORTER
         fi
     elif [[ "$USE_PNPM" == true ]]; then
-        COMMAND_ARGS="-- --reporter $TEST_REPORTER_PATH --outputFile test-report.xml"
+        COMMAND_ARGS="-- --reporter $TEST_REPORTER --outputFile test-report.xml"
         if [[ -d "./node_modules/vitest-sonar-reporter" ]]; then
             echo "Installing $TEST_REPORTER"
             pnpm i -D $TEST_REPORTER
