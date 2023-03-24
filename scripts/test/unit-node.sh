92e7ce854c56963482b08291afd4efcf1c23ed82
Glen Hickman <gchickma@us.ibm.com>
2023-03-24 15:45:15 -0500

    try to fix coverage inclusions and exclusions x2

--- scripts/test/unit-node.sh
+++ scripts/test/unit-node.sh
@@ -105,23 +105,19 @@ if [[ "$TEST_SCRIPT" != "undefined" ]]; then
     if [[ -d "./node_modules/vitest" && "$TEST_SCRIPT" == *vitest* ]]; then
         COVERAGE_PROVIDER="c8"
         COVERAGE_REPORTER="lcov"
-        echo "USER_INCLUSIONS=$USER_INCLUSIONS"
-        COVERAGE_INCLUDE=
-        if [ "$USER_INCLUSIONS" != "undefined" ]; then
-            COVERAGE_INCLUDE=$USER_INCLUSIONS
+        COMMAND_ARGS="-- --coverage.enabled --reporter=$TEST_REPORTER --outputFile=$UNIT_TEST_REPORT_NAME --coverage.reporter=$COVERAGE_REPORTER --coverage.provider=$COVERAGE_PROVIDER --coverage.all=true"
+        echo "USER_INCLUSIONS=[$USER_INCLUSIONS]"
+        if [[ "$USER_INCLUSIONS" != "undefined" && "$USER_INCLUSIONS" != "" ]]; then
+            COMMAND_ARGS="$COMMAND_ARGS --coverage.include=$USER_INCLUSIONS"
         else
-            COVERAGE_INCLUDE="**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"
+            COMMAND_ARGS="$COMMAND_ARGS --coverage.include=src"
         fi
-        echo "USER_EXCLUSIONS=$USER_EXCLUSIONS"
-        COVERAGE_EXCLUDE=
-        if [ "$USER_EXCLUSIONS" != "undefined" ]; then
-            COVERAGE_EXCLUDE=$USER_EXCLUSIONS
-        else
-            COVERAGE_EXCLUDE="**/node_modules/**,**/dist/**,**/cypress/**,**/.{idea,git,cache,output,temp}/**,**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*"
+        echo "USER_EXCLUSIONS=[$USER_EXCLUSIONS]"
+        if [[ "$USER_EXCLUSIONS" != "undefined" && "$USER_EXCLUSIONS" != "" ]]; then
+            COMMAND_ARGS="$COMMAND_ARGS --coverage.exclude=$USER_EXCLUSIONS"
         fi
-        TEST_REPORTER="vitest-sonar-reporter"
-        COMMAND_ARGS="-- --coverage.enabled --reporter=$TEST_REPORTER --outputFile=$UNIT_TEST_REPORT_NAME --coverage.reporter=$COVERAGE_REPORTER --coverage.provider=$COVERAGE_PROVIDER --coverage.include=$COVERAGE_INCLUDE --coverage.exclude=$COVERAGE_EXCLUDE --coverage.all=true"
 
+        TEST_REPORTER="vitest-sonar-reporter"      
         if [[ ! -d "./node_modules/$TEST_REPORTER" ]]; then
             if [[ "$USE_NPM" == true ]]; then
                 echo "Installing $TEST_REPORTER"
