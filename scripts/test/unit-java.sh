b123e57e837fe70190f1d49023cc36d159d9babb
Ming Xia Shi <mingxias@cn.ibm.com>
2024-03-27 12:45:26 +0800

    change maxmataspacesize to 1024

--- scripts/test/unit-java.sh
+++ scripts/test/unit-java.sh
@@ -11,7 +11,7 @@ COMPONENT_NAME=$6
 
 if [ "$BUILD_TOOL" == "maven" ]; then
     echo "Testing with Maven"
-    MAVEN_OPTS="-Xmx1024m -XX:MaxMetaspaceSize=128m"
+    MAVEN_OPTS="-Xmx1024m -XX:MaxMetaspaceSize=1024m"
     if [ "$HTTP_PROXY" != "" ]; then
         # Swap , for |
         MAVEN_PROXY_IGNORE=`echo "$NO_PROXY" | sed -e 's/ //g' -e 's/\"\,\"/\|/g' -e 's/\,\"/\|/g' -e 's/\"$//' -e 's/\,/\|/g'`
