ed3f161a4714ee13643632a5476e63cae58b3542
Glen Hickman <gchickma@us.ibm.com>
2024-04-24 15:52:28 -0500

    add --remote to submodule update

--- scripts/common/git-clone.sh
+++ scripts/common/git-clone.sh
@@ -101,7 +101,7 @@ if  [ -d "$REPO_FOLDER" ]; then
     fi
 
     echo "Git update submodules"
-    git submodule update --init --recursive
+    git submodule update --init --recursive --remote
     GIT_RC=$?
     if [ $GIT_RC != 0 ]; then
         echo "Git update submodules failed"
